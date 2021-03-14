71809e1b0e6654b6da57878fee7d1f0c64977b3f
Josep <Josep@192.168.100.24>
2021-03-14 08:37:49 +0300

    First comment

---
+++ CreditCard/src/CreditCard.java
@@ -0,0 +1,16 @@
+
+public class CreditCard {
+	
+	private int card;
+	
+	//getter
+	public int getCard() {
+		return card;
+	}
+	
+	//setter
+	public void setCard(int newCard) {
+		this.card = newCard;
+	}
+
+}
---
+++ CreditCard/src/TestCard.java
@@ -0,0 +1,12 @@
+
+public class TestCard {
+	public static void main(String[] args) {
+		
+		CreditCard myobj = new CreditCard();
+		
+		myobj.setCard(10995578);
+		
+		System.out.println("This is your CreditCard number: " + myobj.getCard());
+	}
+
+}
